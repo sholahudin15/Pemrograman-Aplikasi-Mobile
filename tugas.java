c1472a4d259442fefefaf76385a9dce2fe12fa5d
sholahudin15 <43536941+sholahudin15@users.noreply.github.com>
2018-09-24 16:59:57 +0700

    Add files via upload

---
+++ tugas.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication6;
+
+/**
+ *
+ * @author TOSHIBA
+ */
+public class JavaApplication6 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int umur=21;
+        String nama="M Sholahudin Tri Y";
+        boolean cantik = true;
+         if(umur>23 && cantik==true){
+           System.out.println("saya mau");
+       }
+       else if(umur>23 ){
+           System.out.println("Saya pikir-pikir dulu");
+       }
+       else {
+           System.out.println("maaf saya tidak mau");
+       }
+    }
+    
+}
